99d1c011f88b091f29e6d81b6d766458551e79af
Tomasz Cichocki <tomasz.cichocki@optiva.com>
2018-12-22 15:14:41 +0100

    add index generation

--- process.py
+++ process.py
@@ -9,10 +9,16 @@ OUTPUT = "t/posts"
 HEAD = "t/layouts/header.php"
 FOOT = "t/layouts/footer.php"
 
-def process(input, outdir):
+def title_from_path(input):
+    return re.sub(r'.md$','', input.name).replace("-", " ")
+
+def short_article_path(input):
+    return re.sub(r'.md$','.html', input.name)
+
+def process(input, index, outdir):
     print(input)
-    fout = re.sub(r'.md$','.html', input.name)
-    title = re.sub(r'.md$','', input.name).replace("-", " ")
+    fout = short_article_path(input)
+    title = title_from_path(input)
     input_file = input.open(mode="r", encoding="utf-8")
     text = input_file.read()
     html = markdown.markdown(text, output_format="html5")
@@ -28,6 +34,7 @@ def process(input, outdir):
   <div class="row">
     <div class="col-12 col-md-3 push-md-9 bd-sidebar">
 """)
+    output_file.write(index)
     output_file.write("""    </div>
     <div class="col-12 col-md-9 pull-md-3 bd-content">
 """)
@@ -38,7 +45,21 @@ def process(input, outdir):
 """)
     output_file.write(codecs.open(FOOT, mode="r", encoding="utf-8").read())
 
-# for f in os.listdir(INPUT):
-for f in Path(INPUT).glob("*.md"):
-    #if f.endswith(".md"):
-    process(f, OUTPUT)
+def generate_index(posts):
+    output = "<p>"
+    if len(posts) > 0:
+        output += "<ul>"
+    for f in posts:
+        output += "<li><a href=\"{path}\">{title}</a></li>".format(
+                        path = short_article_path(f), title = title_from_path(f)
+                )
+    if len(posts) > 0:
+        output += "</ul>"
+    output += "</p>"
+    return output
+
+posts = [p for p in Path(INPUT).glob("*.md")]
+index = generate_index(posts)
+
+for f in posts:
+    process(f, index, OUTPUT)
