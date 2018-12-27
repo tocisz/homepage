9b51ad2cd1c134d4a925ac9808f6996bbcb43c1d
Tomasz Cichocki <tomasz.cichocki@optiva.com>
2018-12-27 22:41:09 +0100

    Use "with" syntax

--- t/process.py
+++ t/process.py
@@ -99,44 +99,37 @@ def upload_text(key, body):
 def process(input, index, outdir):
     fout = short_article_path(input)
     title = title_from_path(input)
-    input_file = input.open(mode="r", encoding="utf-8")
-    text = input_file.read()
+    text = input.open(mode="r", encoding="utf-8").read()
     html = markdown.markdown(
         text,
         extensions = ['extra','meta'],
         output_format="html5"
     )
-    output = StringIO()
-    output.write(
-        codecs.open(
-            HEAD,
-            mode="r",
-            encoding="utf-8"
-        ).read().format(title=title)
-    )
-    output.write("""<div class="container-fluid">
+    with StringIO() as output:
+        output.write(
+            codecs.open(
+                HEAD,
+                mode="r",
+                encoding="utf-8"
+            ).read().format(title=title)
+        )
+        output.write("""<div class="container-fluid">
   <div class="row">
     <div class="col-12 col-md-3 push-md-9 bd-sidebar">
 """)
-    output.write(index)
-    output.write("""    </div>
+        output.write(index)
+        output.write("""    </div>
     <div class="col-12 col-md-9 pull-md-3 bd-content">
 """)
-    output.write(html)
-    output.write("""    </div>
+        output.write(html)
+        output.write("""    </div>
   </div>
 </div>
 """)
-    output.write(codecs.open(FOOT, mode="r", encoding="utf-8").read())
-    # output_file = codecs.open(os.path.join(outdir,fout), "w",
-    #                       encoding="utf-8",
-    #                       errors="xmlcharrefreplace"
-    #                       )
-    # output_file.write(output.getvalue())
+        output.write(codecs.open(FOOT, mode="r", encoding="utf-8").read())
+        body = output.getvalue()
 
     key = outdir + '/' + fout
-    body = output.getvalue()
-    output.close()
     upload_text(key, body)
 
 def upload_file(f, outdir):
@@ -175,34 +168,37 @@ def upload_dir(dir, outdir=None):
             upload_file(f, outdir)
 
 def generate_index(posts, path_prefix=""):
-    output = "<p>"
-    if len(posts) > 0:
-        output += "<ul>"
-    for f in posts:
-        output += "<li><a href=\"{path}\">{title}</a></li>".format(
-                        path = path_prefix+short_article_path(f), title = title_from_path(f)
+    with StringIO() as output:
+        output.write("<p>")
+        if len(posts) > 0:
+            output.write("<ul>")
+        for f in posts:
+            output.write("<li><a href=\"{path}\">{title}</a></li>".format(
+                    path = path_prefix + short_article_path(f),
+                    title = title_from_path(f)
                 )
-    if len(posts) > 0:
-        output += "</ul>"
-    output += "</p>"
-    return output
+            )
+        if len(posts) > 0:
+            output.write("</ul>")
+        output.write("</p>")
+        return output.getvalue()
 
 def generate_frontpage(posts):
     title = 'The blog archive';
-    output = StringIO()
-    output.write(
-        codecs.open(
-            HEAD,
-            mode="r",
-            encoding="utf-8"
-        ).read().format(title=title)
-    )
-    output.write("""<div class="container-fluid">
+    with StringIO() as output:
+        output.write(
+            codecs.open(
+                HEAD,
+                mode="r",
+                encoding="utf-8"
+            ).read().format(title=title)
+        )
+        output.write("""<div class="container-fluid">
 <h1>{}</h1>""".format(title))
-    output.write(generate_index(posts, OUTPUT+"/"))
-    output.write("</div>")
-    output.write(codecs.open(FOOT, mode="r", encoding="utf-8").read())
-    return output.getvalue()
+        output.write(generate_index(posts, OUTPUT+"/"))
+        output.write("</div>")
+        output.write(codecs.open(FOOT, mode="r", encoding="utf-8").read())
+        return output.getvalue()
 
 print("Processing index.html")
 posts = [p for p in Path(INPUT).glob("*.md")]
